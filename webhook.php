92784352a96f16b7ddf1328a00db81e39a69c259
agui1lera <oscar@aguilera.xyz>
2019-07-26 09:15:25 -0400

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -1,5 +1,6 @@
 <?php
 
+$confidence_general=0.7;
 
 //get a message, triggered by app chat api
 
@@ -14,6 +15,7 @@ $flag_loop = $data["messages"]["0"]["fromMe"];
 $pos =  1;
 $pos2 = 1;
 $pos3 = 1;
+$comando=0;
 
 
 $search ='Eli';
@@ -38,11 +40,33 @@ if(preg_match("/{$search}/i", $mensaje)) {
 };
 
 
+$search ='clima';
+if(preg_match("/{$search}/i", $mensaje)) {
+    
+$comando =2;
+};
+
+$search ='Clima';
+if(preg_match("/{$search}/i", $mensaje)) {
+    
+$comando =2;
+};
+
+
+$search ='CLIMA';
+if(preg_match("/{$search}/i", $mensaje)) {
+ 
+$comando =2;
+	
+};
+
+
+
 
 //echo $mensaje;
-echo "pos".$pos."</BR>";
-echo "pos2".$pos2."</BR>";
-echo "pos3".$pos3."</BR>";
+//echo "pos".$pos."</BR>";
+//echo "pos2".$pos2."</BR>";
+//echo "pos3".$pos3."</BR>";
 if (($pos == 0) or ($pos2 == 0) or ($pos3 == 0))
     
 {
@@ -63,23 +87,34 @@ curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonDataEncoded);
 curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json')); 
 $result = curl_exec($ch);
 $resultDecode =json_decode($result);
-
+$data=json_decode($result,true);
 
 $respuesta = $resultDecode->{'output'}->{'text'}[0];
-$confidence = $resultDecode->{'output'}->{'text'}[0];
-
- 
-echo "Respuesta: ".$respuesta."</BR>";
-
-echo $confidence;
 
 
 
-if ($flag_loop == 0)
+$confidence =$data['intents'][0]['confidence'];
+
+$conversation_id = $data['extities']['output']['context']['conversation_id'];
+//echo "<br>conversation_id:" ."  " . $conversation_id;
+$dialog_turn_counter = $data['extities']['output']['context']['system']['dialog_turn_counter'];
+//echo "<br>dialog_turn_counter:" ."  " . $dialog_turn_counter;
+
+$confidence2 =(float)$confidence ;
+echo "confidence:" ."  " . $confidence2;
+echo "---repuesta  ".$respuesta;
+
+
+
+if ($confidence2 > $confidence_general)
+{
+
+
+
+if ($flag_loop == 0) 
 	
-	{
-
-		$url = 'https://eu48.chat-api.com/instance51261/message?token=7ur7q8g8ayov0chi';
+	{	
+        $url = 'https://eu57.chat-api.com/instance54979/message?token=w2pbubkfb1ga58ot';	
 		$ch = curl_init($url);
 		$jsonData = array(
 			'chatId'=>$id,
@@ -90,8 +125,70 @@ if ($flag_loop == 0)
 		curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonDataEncoded);
 		curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json')); 
 		$result = curl_exec($ch);
-		echo $result;
+		echo "Mensaje enviado";
 	
     }
-	}
+}
+else
+		
+	{
+	    $url = 'https://eu57.chat-api.com/instance54979/message?token=w2pbubkfb1ga58ot';
+    	$ch = curl_init($url);
+		$jsonData = array(
+			'chatId'=>$id,
+			'body' =>"Mi respuesta no tiene el nivel de confianza , pero aprenderé para entregartela");
+     
+		$jsonDataEncoded = json_encode($jsonData);
+		curl_setopt($ch, CURLOPT_POST, 1);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonDataEncoded);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json')); 
+		$result = curl_exec($ch);
+		echo "Mensaje enviado 2";
+	};
+	
+ if ($comando==2)
+    {
+		
+	$url = 'http://api.meteoagro.co/v1?apikey=LXASZZNJFM3MQ0N6KWCK&idstation=CHIRECU62';
+	$ch = curl_init($url);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: x-www-form-urlencoded'));
+	//$result = curl_exec($ch);
+
+	$result = file_get_contents($url);
+	// Will dump a beauty json :3
+	$jsonobject = json_decode($result, true);
+
+	echo "wind_speed_kmh : ";
+
+	$velocidad_del_viento=$jsonobject['CurrentDataClimatic']['wind_speed_kmh'];
+	echo $velocidad_del_viento;
+	echo "<br/>";
+	echo "temp_c : ";
+	$temperatura= $jsonobject['CurrentDataClimatic']['temp_c'];
+	echo $temperatura;
+	$data_clima="En Melipilla Temperatura: ".$temperatura."° Velocidad del viento: ".$velocidad_del_viento. "KM/H";
+	
+	
+	
+        $url = 'https://eu57.chat-api.com/instance54979/message?token=w2pbubkfb1ga58ot';
+    	$ch = curl_init($url);
+		$jsonData = array(
+			'chatId'=>$id,
+			'body' =>$data_clima);
+     
+		$jsonDataEncoded = json_encode($jsonData);
+		curl_setopt($ch, CURLOPT_POST, 1);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonDataEncoded);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json')); 
+		$result = curl_exec($ch);
+		echo "Mensaje enviado 3";	
+	  	
+		
+    }
+	
+
+}
+
+
+
 ?>
